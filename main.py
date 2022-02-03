49557236af79c967643aef518fe54fc97d574121
Ilya <ilya.panshenskov@italki.com>
2022-02-03 11:43:01 +0800

    all systems go

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from selenium import webdriver
+import scraper
 import os
+from sqlalchemy import create_engine
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+selenium_driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+db_access_key = os.environ.get("POSTGRE")
 
-# Now you can start using Selenium
-driver.get("https://www.wellcee.com/rent-apartment/shanghai/list?cityId=15102233103895305#rentTypeIds=15103931190241306")
-print(driver.page_source[0:100])
+if __name__ == "__main__":
+    cnx = create_engine(db_access_key)
+
+    print('Scraping in progress...', end = ' ')
+
+    listing_list_result = scraper.scrape_wellcee_listings(selenium_driver)
+
+    df = scraper.scrape_wellcee_data(listing_list_result)
+
+    print('Scraping Done!')
+
+    df.to_sql('wellcee_listings', cnx, schema = 'public', index = False, chunksize=100, if_exists='replace', method = 'multi')
+
+    print('Data uploaded!')
