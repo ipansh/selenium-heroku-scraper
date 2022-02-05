0a7d9cd92357601701361ba39c741cad385f14a8
Ilya <76761738+ipansh@users.noreply.github.com>
2022-02-05 17:18:40 +0800

    combining datasets

--- main.py
+++ main.py
@@ -27,10 +27,10 @@ if __name__ == "__main__":
     listing_list_result = scraper.scrape_wellcee_listings(selenium_driver)
     print(listing_list_result)		
     df = scraper.scrape_wellcee_data(listing_list_result)
-    final_df = pd.concat([current_df,df]).drop_duplicates().drop(columns = ['id']).reset_index().rename(columns = {'index':'id'})
-    print(final_df)
-    final_df.to_sql('wellcee_listings', cnx, schema = 'public', index = False, chunksize=100, if_exists='replace', method = 'multi')
-
+    wellcee_df = pd.concat([current_df,df]).drop_duplicates().drop(columns = ['id']).reset_index().rename(columns = {'index':'id'})
+    print(wellcee_df)
+    wellcee_df.to_sql('wellcee_listings', cnx, schema = 'public', index = False, chunksize=100, if_exists='replace', method = 'multi')
+    wellcee_df['website'] = 'wellcee'
     print('Wellcee data uploaded!')
 
     ### SMARTSHANGHAI
@@ -43,11 +43,17 @@ if __name__ == "__main__":
 
     listing_list_result = scraper.scrape_smartshanghai_listing(3)
     print(listing_list_result)		
-    df = scraper.scrape_smartshanghai_data(listing_list_result)
-    final_df = pd.concat([current_df,df]).drop_duplicates().drop(columns = ['id']).reset_index().rename(columns = {'index':'id'})
-    print(final_df)
-    final_df.to_sql('smart_shanghai_listings', cnx, schema = 'public', index = False, chunksize=100, if_exists='replace', method = 'multi')
+    smartshanghai_df = scraper.scrape_smartshanghai_data(listing_list_result)
+    smartshanghai_df = pd.concat([current_df,df]).drop_duplicates().drop(columns = ['id']).reset_index().rename(columns = {'index':'id'})
+    print(smartshanghai_df)
+    smartshanghai_df.to_sql('smart_shanghai_listings', cnx, schema = 'public', index = False, chunksize=100, if_exists='replace', method = 'multi')
+    smartshanghai_df['website'] = 'smartshanghai'
 
     print('Smart Shanghai data uploaded!')
 
-    telegram_notifier.send_message('Job done, my lord!', telegram_token)
+    ##COMBINING
+    all_together_df = smartshanghai_df.append(wellcee_df)
+    all_together_df['id'] = list(range(0,len(all_together_df)))
+    all_together_df.to_sql('all_listings', cnx, schema = 'public', index = False, chunksize=100, if_exists='replace', method = 'multi')
+
+    telegram_notifier.send_message('Job done!', telegram_token)
